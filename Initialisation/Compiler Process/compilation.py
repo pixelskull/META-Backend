c559078b47cc68acc53882f32a27c7deda739fa2
pixelskull <pascal.schoenthier@th-koeln.de>
2017-06-14 15:35:18 +0200

    Streamlined compilation.py and added arguments
    - Added new argument handling in compilation.py
    - process for compilation now streamlined (works now only with IR files)
    - removed support for swift files

--- Initialisation/Compiler Process/compilation.py
+++ Initialisation/Compiler Process/compilation.py
@@ -3,43 +3,98 @@
 import sys
 import os
 
+import argparse
+
+def prepareParser():
+    parser = argparse.ArgumentParser(description="""compilation.py takes an LLVM-IR file and compiles
+                                                     this one to executable code""")
+
+    parser.add_argument("IR_File",
+                        type=str,
+                        help="ir file to compile with compilation.py")
+
+    parser.add_argument("-v", "--verbose",
+                        help="increase output verbosity",
+                        action="store_true")
+    parser.add_argument("-c", "--clean",
+                        help="removes all temporary files after build is finished",
+                        default=True,
+                        action="store_true")
+    parser.add_argument("-t", "--target",
+                        help="define triple target for llvm compiler",
+                        action="store",
+                        default="arm64-apple-ios10.0",
+                        dest="target_value")
+    parser.add_argument("-s", "--sdk",
+                        help="define sdk to use with xcrun",
+                        action="store",
+                        default="iphoneos",
+                        dest="sdk_value")
+    parser.add_argument("-o", "--output",
+                        help="define the output name used for storing file to disc",
+                        action="store",
+                        default="a.out",
+                        dest="output_name")
+
+    return parser
+
+
 def main(argv):
-    program_name = "python_test.out" #TODO hier noch den namen als parameter einlesen
+    # prepare argument parser and parse the argv
+    parser = prepareParser()
+    args = parser.parse_args()
 
-    sdk = "xcrun -sdk iphoneos" #TODO hier noch anpassungen vornehmen, damit die sdk geãndert werden kann
+    print(args.IR_File)
 
-    input_file = "hello.swift" #TODO hier noch die richtige file angeben
-    output_file = "hello_py.ll" #TODO hier noch einen generischen output namen wählen
-    print("-> compiling file for iOS...")
-    os.system(sdk + ' swiftc -emit-ir -target arm64-apple-ios10.0 ' + input_file + ' -o ' + output_file)
+    # sdk = "xcrun -sdk " + args.sdk_value
 
-    ir_file = "output.ll" #TODO hier noch einen generischen output namen wählen
-    print("-> transforming IR Code for macOS...")
-    os.system('./ir-transformator.py ' + output_file)
+    # input_file = "hello.swift"
+    # output_file = "hello_py.ll"
+    # print("-> compiling file for iOS...")
+    # os.system(sdk + ' swiftc -emit-ir -target arm64-apple-ios10.0 ' + input_file + ' -o ' + output_file)
 
-    transformed_ir_file = "output.bc" #TODO hier noch einen generischen output namen wählen
-    print("-> transforming to LLVM-Bitcode...")
-    os.system('llvm-as ' + ir_file)
+    # transformation from iOS based IR to macOS based IR
+    ir_file = args.IR_File.replace(' ', '\ ')
+    if args.verbose:
+        print("-> transforming IR Code for macOS...")
+    os.system('./ir-transformator.py ' + ir_file) # TODO: change output path here to custom path or tempDir
 
-    object_file = "output.o" #TODO hier noch einen generischen output namen wählen
-    print("-> compiling to linkable object file...")
+    # assambling IR code to bitcode
+    transformed_ir_file = "output.bc"
+    if args.verbose:
+        print("-> transforming to LLVM-Bitcode...")
+    os.system('llvm-as output.ll')
+
+    # generating object file from bitcode via llc (low level compiler)
+    object_file = "output.o"
+    if args.verbose:
+        print("-> compiling to linkable object file...")
     os.system('llc -filetype=obj ' + transformed_ir_file)
 
-    print("-> compiling object files to executable...")
-    os.system('swiftc -o ' + program_name + ' ' + object_file)
+    # compiling to executable
+    if args.verbose:
+        print("-> compiling object files to executable...")
+    os.system('swiftc -o ' + args.output_name + ' ' + object_file)
 
-    print("-> finished compilation chain!")
-    print("")
+    if args.verbose:
+        print("-> finished compilation chain!")
+        print("")
 
-    # Removing temp files
-    print("-> removing IR files")
-    os.system('rm ' + output_file + ' ' + ir_file)
+    if args.clean:
+        # Removing temp files
+        if args.verbose:
+            print("-> removing IR files")
+        # os.system('rm ' + output_file + ' ' + ir_file)
+        os.system('rm output.ll')
 
-    print("-> removing bitcode file")
-    os.system('rm ' + transformed_ir_file)
+        if args.verbose:
+            print("-> removing bitcode file")
+        os.system('rm ' + transformed_ir_file)
 
-    print("-> removing object file")
-    os.system('rm ' + object_file)
+        if args.verbose:
+            print("-> removing object file")
+        os.system('rm ' + object_file)
+
 
 # starting main method
 if __name__ == '__main__':
