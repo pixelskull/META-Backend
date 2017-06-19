95cc9e15292377f202e2dc82ec97091f25b69cdc
pixelskull <pascal.schoenthier@th-koeln.de>
2017-06-19 14:38:17 +0200

    Readded Xcode Terminal App Boilerplate

---
+++ ComputeUnitHandler/ComputeUnitHandler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ComputeUnitHandler
+//
+//  Created by Pascal Schönthier on 16.06.17.
+//  Copyright © 2017 Pascal Schönthier. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
