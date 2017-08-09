2bcab0cb977f4f40850b7d0369cf9a9c99e32f2f
pixelskull <pascal.schoenthier@th-koeln.de>
2017-08-09 13:56:39 +0200

    Refactoring of meta-compilation-service.py
    -> Changed JSON Key to begin with uppercase letter

--- Initialisation/Compiler Process/meta-compilation-service.py
+++ Initialisation/Compiler Process/meta-compilation-service.py
@@ -114,10 +114,10 @@ def subscriber():
 def callback_subscriber(ch, method, properties, body):
     json_data = json.loads(body)
 
-    object_path = compile_file(json_data["id"], json_data["content"])
-    service_path = compile_service(object_path, json_data["id"])
+    object_path = compile_file(json_data["Id"], json_data["Content"])
+    service_path = compile_service(object_path, json_data["Id"])
 
-    json_data["content"] = service_path
+    json_data["Content"] = service_path
     json_string = json.dumps(json_data)
 
     publish(json_string)
